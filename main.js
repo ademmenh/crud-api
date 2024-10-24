440fbc853e48a6b194c1cf2724851e311c922324
AdemMENHOUR <150479331+ademmenh@users.noreply.github.com>
2024-10-24 12:32:16 +0100

    adding GET /adding2
    this route expect many numeric params
    and return the sum of them

--- main.js
+++ main.js
@@ -9,10 +9,31 @@ app.get('/hello', (req, res) => {
     res.send('hellooooooo')
 })
 
+
+
 app.get('/adding/:num1/:num2', (req, res) => {
     console.log(`GET /adding/${req.params.num1}/${req.params.num2}`)
     res.send(`${Number(req.params.num1) + Number(req.params.num2)}`)
 })
+
+
+
+app.get('/adding2', (req, res) => {
+
+    let queryparams = '?'
+    for ([key, value] of Object.entries(req.query)){
+        queryparams += `${key}:${value} `
+    }
+    console.log(`GET /adding2${queryparams}`)
+
+    let result = 0
+    for ([key, value] of Object.entries(req.query)){
+        result += Number(value)
+    }
+    res.send(`${result}`)
+})
+
+
 
 app.listen(8000, () => {
     console.log('the server is running on the porst 8000 !')
