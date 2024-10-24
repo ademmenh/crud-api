f8627cefba7a8ec00cf5373fa80b31acaf33d9bf
AdemMENHOUR <150479331+ademmenh@users.noreply.github.com>
2024-10-25 00:13:18 +0100

    editing GET /adding2 route
    in main.js, in Get /adding2
    tracking the requests on the log with there body params,

--- main.js
+++ main.js
@@ -4,6 +4,9 @@ const express = require('express')
 
 const app = express()
 
+
+app.use(express.json())
+
 app.get('/hello', (req, res) => {
     console.log('GET /hello')
     res.send('hellooooooo')
@@ -19,18 +22,35 @@ app.get('/adding/:num1/:num2', (req, res) => {
 
 
 app.get('/adding2', (req, res) => {
-
-    let queryparams = '?'
-    for ([key, value] of Object.entries(req.query)){
-        queryparams += `${key}:${value} `
-    }
-    console.log(`GET /adding2${queryparams}`)
-
     let result = 0
-    for ([key, value] of Object.entries(req.query)){
-        result += Number(value)
+    let bodyParams = Object.entries(req.body);
+
+    if (bodyParams.length === 0) {
+        console.log('GET /adding2')
+        res.send(`${result}`);
+        return;
     }
+    
+    let stringParams = '?'
+    if (bodyParams.length === 1)
+    {
+        stringParams += `=${bodyParams[0][0]}=${bodyParams[0][1]}`
+        console.log(`GET /adding2${stringParams}`)
+        result = bodyParams[0][1];
+        res.send(`${result}`)
+        return;
+    }
+
+    // else
+    stringParams += `${bodyParams[0][0]}=${bodyParams[0][1]}`
+    result = Number(bodyParams[0][1])
+    for (let i=1; i<bodyParams.length; i++) {
+        stringParams += `&${bodyParams[i][0]}=${bodyParams[i][1]}`
+        result += Number(bodyParams[i][1])
+    }
+    console.log(`GET /adding2${stringParams}`)
     res.send(`${result}`)
+    return;
 })
 
 
