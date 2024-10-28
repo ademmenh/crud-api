63258976aac2bc73daeacb2649324f5ace7ca01a
AdemMENHOUR <150479331+ademmenh@users.noreply.github.com>
2024-10-28 23:59:53 +0100

    edii config

--- config/init.js
+++ config/init.js
@@ -1,7 +1,14 @@
 
+import dotenv from 'dotenv'
 import {app} from '../main.js'
+import mongoose from 'mongoose'
 
-export const initServer = () => {
-    const PORT = 8000;
+dotenv.config()
+const PORT = env.PORT
+const DB_URI = process.env.DB_URI
+
+export const initServer = async () => {
+
+        mongoose.connect(DB_URI)
     app.listen(PORT, () => {console.log(`Server listens at port ${PORT}...`)})
 }
